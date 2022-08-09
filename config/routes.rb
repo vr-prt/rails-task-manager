ef49c527a883fe78e26c352154ccac56440ce76f
Victor Prete <vprete@tuta.io>
2022-08-09 12:19:27 -0400

    Add create option for tasks

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/', to: 'tasks#index', as: :tasks
-  get '/task/:id', to: 'tasks#show', as: :task
-
-  get 'task/new', to: 'tasks#new'
+  get '/tasks', to: 'tasks#index', as: :tasks
+  get 'tasks/new', to: 'tasks#new'
+  get '/tasks/:id', to: 'tasks#show', as: :task
+  post 'tasks', to: 'tasks#create'
 end
