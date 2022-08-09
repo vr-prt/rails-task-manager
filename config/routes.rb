6432223be8cae9adefda8082cabcfbb654c67f6e
Victor Prete <vprete@tuta.io>
2022-08-09 12:06:58 -0400

    Add task model and index page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/', to: 'tasks#index', as: :tasks
+  get '/task/:id', to: 'tasks#show', as: :task
+
+  get 'task/new', to: 'tasks#new'
 end
